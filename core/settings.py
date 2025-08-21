aaed882696ca219a4cdac96dea8a0bd7c4f7c9c3
Luis Miguel <luismiguelsantossouza@gmail.com>
2025-08-20 22:07:10 -0300

    fix health test

--- core/settings.py
+++ core/settings.py
@@ -36,7 +36,7 @@ DEBUG = config("DEBUG", default=True, cast=bool)
 
 ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS",
-    default="localhost,127.0.0.1",
+    default="*" if not DEBUG else "localhost,127.0.0.1",
     cast=lambda v: [s.strip() for s in v.split(",")],
 )
 
